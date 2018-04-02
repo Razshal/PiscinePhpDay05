97c23550ae5dd4cf44b55536efeac2f260f8074f
Razshal <fontenille.mathieu@gmail.com>
2018-04-02 20:05:47 +0200

    ex07 corr

--- ex07/ex07.sql
+++ ex07/ex07.sql
@@ -1,1 +1,1 @@
-SELECT title, summary FROM db_mfonteni.film WHERE summary OR title LIKE '%42%' ORDER BY duration;
+SELECT title, summary FROM db_mfonteni.film WHERE summary LIKE '%42%' OR title LIKE '%42%' ORDER BY duration;
